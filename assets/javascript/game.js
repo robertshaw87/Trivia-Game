64d0eeedb216546f2fd8246ba58fc9f7a11c1fe5
Robert Shaw <robertshaw87@gmail.com>
2018-05-02 15:50:25 -0800

    add utility functions, hooks to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,11 @@
 // declare some variables
 var playerScore, curQuestion, questionsList;
 
+var difficultySelect = {
+    0:questionsBank,
+    1:questionsBank2
+}
+
 // returns a random integer between 0 and the argument(inclusive)
 function randInt(maxInt){
     return (Math.floor(Math.random() * (maxInt+1)))
@@ -18,8 +23,16 @@ function shuffleArr(arr) {
     return arr;
 }
 
-function questionArea(str) {
-    $("#question-area").html(str);
+function generateButton(str, id, value=0){
+    return '<button type="button" class="btn btn-dark btn-lg p-3 pr-5 pl-5" id='+id+' value='+value+'>'+str+'</button>';
+}
+
+function titleArea(str) {
+    $("#title-area").html(str);
+}
+
+function messageArea(str) {
+    $("#message-area").html(str);
 }
 
 // reset the game, showing the default home screen
@@ -30,8 +43,11 @@ function resetGame() {
         incorrect: 0,
         skipped: 0};
     questionsList = [];
-    questionArea("<h1 class='text-center'>Trivia Game</h1>");
-    console.log(playerScore)
+    titleArea("<h1 class='text-center'>Trivia Game</h1>");   
+    messageArea('<p>Welcome the to trivia game! Select your difficulty. </p>');
+    buttonsArea('');
+    console.log(generateButton("wee","b"))
+    console.log(playerScore);
 }
 
 // convert a question object into something that will add its own
