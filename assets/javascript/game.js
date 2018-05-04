3dc3dee56116b2cc9a04c607c189db0d8948ca24
Robert Shaw <robertshaw87@gmail.com>
2018-05-03 20:30:22 -0700

    move some global variables into object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,17 +3,50 @@
 // object to store the player's score
 var playerScore = {
     correct: 0,
-    incorrect: 0,
-    skipped: 0
-}
+    wrong: 0,
+    skipped: 0}
 
-var curQuestion, questionsList, questionsIndex;
-
-
-var questions, maxQuestions;
-maxQuestions = 3;
+var questions = {
+    questionsIndex : 0,
+    maxQuestions: 3,
+    questionsList : [],
+    curQuestion: false}
 
 var difficultySelect = [questionsBank, questionsBank2];
+
+// Timer object for the questions countdown
+// time allotted is how much time i want to give the player per question
+var timer = {
+    timeAllotted: 2,
+    timerCount: 0,
+    timerPointer: false,
+    start: function() {
+        clearInterval(timer.timerPointer);
+        timer.timerCount = timer.timeAllotted;
+        timer.timerPointer = setInterval(timer.decrement, 1000);
+        $("#message-area").html($("<h5>").html("Time Remaining: " + timer.timerCount));
+    },
+    stop: function() {
+        clearInterval(timer.timerPointer);
+    },
+    // this is the timer setup
+    decrement: function() {
+        console.log(timer.timerCount);
+        console.log(timer)
+        timer.timerCount -= 1;
+        if (timer.timerCount<0){
+            $("#buttons-area").empty()
+            $("#message-area").html("Time's up!")
+            timer.stop();
+            playerScore.skipped += 1;
+            // reduce the wrong count because we're increasing it on the function call
+            playerScore.wrong -= 1;
+            setTimeout(questionWrong, 1000);
+        } else {
+            $("#message-area").html($("<h5>").html("Time Remaining: " + timer.timerCount));
+        }
+    }
+}
 
 // returns a random integer between 0 and the argument(inclusive)
 function randInt(maxInt){
@@ -31,61 +64,25 @@ function shuffleArr(arr) {
     return arr;
 }
 
-// Timer object for the questions countdown
-// time allotted is how much time i want to give the player per question
-
-var timer = {
-    timeAllotted: 3,
-    timerCount: 0,
-    timerPointer: false,
-    start: function() {
-        clearInterval(timer.timerPointer);
-        timer.timerCount = timer.timeAllotted;
-        timer.timerPointer = setInterval(timer.decrement, 1000);
-        $("#message-area").html($("<h5>").html("Time Remaining: " + timer.timerCount));
-    },
-    stop: function() {
-        clearInterval(timer.timerPointer);
-    },
-    decrement: function() {
-        console.log(timer.timerCount);
-        console.log(timer)
-        timer.timerCount -= 1;
-        if (timer.timerCount<0){
-            $("#buttons-area").empty()
-            $("#message-area").html("Time's up!")
-            timer.stop();
-            playerScore.skipped += 1;
-            setTimeout(questionWrong, 1000);
-        } else {
-            $("#message-area").html($("<h5>").html("Time Remaining: " + timer.timerCount));
-        }
-    }
-}
-
-
 // generate a centered button div
     // takes in the string and the class you want on the button
     // will carry one data value that defaults to false if not provided
     // 4th argument will be the id you want for the button, also optional
 function generateButton(str, btnClass, data=false, btnID=""){
+    // make the wrapper we're returning that centers the button
     var tempButtonWrap = $("<div>").attr("class", "row mt-3 bg-secondary");
+    // this is the actual button
     var tempButton = $("<div>").attr("class","button btn-dark btn-lg col-10 p-3");
     tempButton.addClass(btnClass);
     tempButton.attr("id", btnID);
     tempButton.attr("data-correct", data);
     tempButton.text(str);
+    // add the padding on both sides of the button
     tempButtonWrap.prepend($("<div>").addClass("col"));
     tempButtonWrap.append(tempButton);
     tempButtonWrap.append($("<div>").addClass("col"));
     return tempButtonWrap;
-
-    // return '<div class="row mt-3 bg-secondary"><div class="col"></div><div class="button btn-dark btn-lg col-10 p-3 '
-    //     + btnClass + '" data-correct='
-    //     + correct + '>' 
-    //     + str + '</div><div class="col"></div></div>';
 }
-
 
 // reset the game, showing the default home screen
 // set all the variables
@@ -93,10 +90,14 @@ function generateButton(str, btnClass, data=false, btnID=""){
 function resetGame() {
     playerScore = {
         correct: 0,
-        incorrect: 0,
+        wrong: 0,
         skipped: 0};
-    questionsList = [];
-    questionsIndex = 0;
+    questions = {
+        questionsIndex : 0,
+        maxQuestions: 3,
+        questionsList : [],
+        curQuestion: false
+    }
     $("#title-area").html("<h1 class='text-center'>Trivia Game</h1>");   
     $("#message-area").html('<p>Welcome the to trivia game! Select your difficulty. </p>');
     for (var i=0; i<difficultySelect.length; i++){
@@ -107,32 +108,36 @@ function resetGame() {
 
 // find the next question in the array and display it
 function nextQuestion() {
-    curQuestion = questionsList[questionsIndex];
-    console.log(curQuestion);
-    $("#buttons-area").empty();
-    $("#message-area").empty();
-    $("#title-area").html($("<h2>").html(curQuestion.q));
-    shuffleArr(curQuestion.s);
-    console.log(curQuestion.s);
-    var solutionNum = randInt(3);
-    for (var i=0; i<4; i++){
-        if (i !== solutionNum){
-            $("#buttons-area").append(generateButton(curQuestion.s[i],"answer-button", false));
-        } else {
-            $("#buttons-area").append(generateButton(curQuestion.a,"answer-button", true, "solution-button"));
+    if (questions.questionsIndex < questions.maxQuestions) {
+        questions.curQuestion = questions.questionsList[questions.questionsIndex];
+        $("#buttons-area").empty();
+        $("#message-area").empty();
+        $("#title-area").html($("<h2>").html(questions.curQuestion.q));
+        shuffleArr(questions.curQuestion.s);
+        console.log(questions.curQuestion.s);
+        var solutionNum = randInt(3);
+        for (var i=0; i<4; i++){
+            if (i !== solutionNum){
+                $("#buttons-area").append(generateButton(questions.curQuestion.s[i],"answer-button", false));
+            } else {
+                $("#buttons-area").append(generateButton(questions.curQuestion.a,"answer-button", true, "solution-button"));
+            }
         }
+        questions.questionIndex--;
+        timer.start();
+    } else {
+        completeGame();
     }
-    timer.start();
 }
 
+// page display on incorrect answer
 function questionWrong() {
+    playerScore.wrong -= 1;
     $("#message-area").empty();
-    // var tempDiv = $("<h4>").attr("class", "text-center");
-    // tempDiv.html();
-    $("#message-area").html(($("<h4>").attr("class", "text-center")).html("The correct answer was " + curQuestion.a));
+    $("#message-area").html(($("<h4>").attr("class", "text-center")).html("The correct answer was " + questions.curQuestion.a));
     $("#buttons-area").empty();
-    $("#buttons-area").html(curQuestion.blurb);
-
+    $("#buttons-area").html(questions.curQuestion.blurb);
+    setTimeout(nextQuestion, 3000);
 }
 
 function questionRight() {
@@ -149,10 +154,10 @@ $(document).ready(function() {
         // save the this pointer in order to easily reference
         var currButton = $(this);
         // select the questions associated with the difficulty selected
-        questionsList = difficultySelect[parseInt(currButton.attr("data-correct"))].qList;
+        questions.questionsList = difficultySelect[parseInt(currButton.attr("data-correct"))].qList;
         // // randomize the questions
-        shuffleArr(questionsList);
-        console.log(questionsList);
+        shuffleArr(questions.questionsList);
+        console.log(questions.questionsList);
         nextQuestion();
     });
 
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,9 +1,12 @@
 var questionsBank = {
     name: "Easy Questions",
     qList: [
-        {q:"question 1", a:"answer 1", s:["wrong 1-1", "wrong 1-2", "wrong 1-3", "wrong 1-4"]},
-        {q:"question 2", a:"answer 2", s:["wrong 2-1", "wrong 2-2", "wrong 2-3", "wrong 2-4", "wrong 2-5"]},
-        {q:"question 3", a:"answer 3", s:["wrong 3-1", "wrong 3-2", "wrong 3-3", "wrong 3-4", "wrong 3-5"]}]
+        {q:"question 1", a:"answer 1", s:["wrong 1-1", "wrong 1-2", "wrong 1-3", "wrong 1-4"], 
+            blurb:"<h2 class='text-center'>lorem ipsum</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu ultricies felis. Praesent eget elit volutpat, sodales risus a, iaculis nibh. Nunc scelerisque, enim ac sodales lacinia, neque lorem mollis dui, ac viverra sem enim lacinia velit. </p>"},
+        {q:"question 2", a:"answer 2", s:["wrong 2-1", "wrong 2-2", "wrong 2-3", "wrong 2-4", "wrong 2-5"],
+            blurb:"<h2 class='text-center'>lorem ipsum</h2><p>auris turpis nisi, dapibus quis sagittis et, porttitor vitae lectus. Donec eu elit lectus. Vivamus vitae volutpat augue. Cras a justo nulla. Donec ornare consectetur urna. Aliquam erat volutpat. Pellentesque ornare porta enim, id tempus enim luctus sit amet.</p>"},
+        {q:"question 3", a:"answer 3", s:["wrong 3-1", "wrong 3-2", "wrong 3-3", "wrong 3-4", "wrong 3-5"],
+            blurb:"<h2 class='text-center'>lorem ipsum</h2><p>Morbi aliquam convallis nunc quis laoreet. Phasellus porta ac neque aliquet aliquam. In hac habitasse platea dictumst. Etiam rutrum nulla eleifend, pharetra libero non, malesuada odio.</p>"}]
 }
 
 var questionsBank2 = {
